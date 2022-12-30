e361704168683eb9c15d686f5fd8a05482834e78
Doni Hadimas <donihadimasaprilian@gmail.com>
2022-12-30 22:21:39 +0700

    feat(public) : add custom css & bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,19 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+     <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('vendor/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
+    <link href="{{ asset('css/demo.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/bootstrap.bundle.js') }}" ></script>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     @inertiaHead
   </head>
   <body>
     @inertia
+    <script src="{{ asset('js/popper.min.js') }}" ></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
   </body>
 </html>
